370e2c33dd1635de3dd16a133a0347985ad871e0
Your Name <you@example.com>
2023-07-03 04:40:50 -0400

    Complete

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,29 +3,49 @@ class BirdsController < ApplicationController
   # GET /birds
   def index
     birds = Bird.all
-    render json: birds
+    render json: birds, except: [:created_at, :updated_at]
   end
 
   # POST /birds
   def create
     bird = Bird.create(bird_params)
-    render json: bird, status: :created
+    render json: bird , except: [:created_at, :updated_at], status: :created
   end
 
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
     if bird
-      render json: bird
+      render json: bird, except: [:created_at, :updated_at]
     else
       render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  def update
+    bird = Bird.find_by(id:params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird, except: [:created_at, :updated_at], status: :accepted
+    else
+      render json: {error: "bird not updated"}, status: :not_found
+    end
+  end
+
+  def increment_likes
+    bird = Bird.find_by(id:params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird, except: [:created_at, :updated_at], status: :accepted
+    else
+      render json: {error: "bird not updated"}, status: :not_found
     end
   end
 
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
